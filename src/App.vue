ac8a034fa848f800867d7e4851e73667ca61e76f
Dave Staab <dave.staab@cadre5.com>
2018-11-10 14:23:34 -0500

    add the basic red planet

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-
+    <!-- step 1: the red planet -->
+    <svg width="500" height="500" class="space">
+      <g transform="translate(250, 250)">
+        <circle r="200" fill="firebrick"></circle>
+      </g>
+    </svg>
   </div>
 </template>
 
