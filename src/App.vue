d46b2ce14d1a70ee9464ce5316fac0f373105b80
Dave Staab <dave.staab@cadre5.com>
2018-11-10 22:59:44 -0500

    iterating over planets and drawing circle

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,29 @@
   <div id="app">
     <!-- step 1: the red planet -->
     <svg-centered>
-      <circle r="200" fill="firebrick"></circle>
+      <circle v-for="l in Earth.layers" :key="l.name" :r="scale(l.radius)" :fill="l.color"></circle>
     </svg-centered>
-
+    <pre>{{scale(2010)}}</pre>
+    <pre>{{Earth}}</pre>
   </div>
 </template>
 
 <script>
 import SvgCentered from './SvgCentered';
+import { Earth } from './planets.js';
+import { scaleLinear } from 'd3-scale';
+const scale = scaleLinear()
+  .domain([0, 4025])
+  .range([0, 200]);
+
 export default {
-  components: { SvgCentered }
+  components: { SvgCentered },
+  data() {
+    return {
+      Earth,
+      scale
+    };
+  }
 };
 </script>
 
