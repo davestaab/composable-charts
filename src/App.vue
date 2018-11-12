3c4cb8d09e2bb6dbaf34c3fe1a1ac25ccc8a9b11
Dave Staab <dave.staab@cadre5.com>
2018-11-12 06:54:58 -0500

    adding first step of lesson 2

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
   <div id="app">
     <!-- step 1: the red planet -->
     <svg-centered>
-      <circle v-for="l in Earth.layers" :key="l.name" :r="scale(l.radius)" :fill="l.color"></circle>
+      <circle :r="Earth.layers[0].radius" fill="firebrick"></circle>
     </svg-centered>
-    <pre>{{scale(2010)}}</pre>
     <pre>{{Earth}}</pre>
   </div>
 </template>
@@ -12,17 +11,12 @@
 <script>
 import SvgCentered from './SvgCentered';
 import { Earth } from './planets.js';
-import { scaleLinear } from 'd3-scale';
-const scale = scaleLinear()
-  .domain([0, 4025])
-  .range([0, 200]);
 
 export default {
   components: { SvgCentered },
   data() {
     return {
-      Earth,
-      scale
+      Earth
     };
   }
 };
@@ -35,5 +29,8 @@ body {
 }
 .space {
   background-color: black;
+}
+pre {
+  font-size: 20px;
 }
 </style>
