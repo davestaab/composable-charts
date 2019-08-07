71c77f289e6f2025c897e5b48cf536c40d3f971e
Dave Staab <davestaab@gmail.com>
2019-08-07 09:47:14 -0400

    removed centeredSvg and added centered

---
+++ src/Centered.vue
@@ -0,0 +1,20 @@
+<template>
+  <g :transform="translate(size / 2, size / 2)">
+    <slot></slot>
+  </g>
+</template>
+<script>
+import { translate } from './utils';
+
+export default {
+  props: {
+    size: {
+      type: Number,
+      default: 500
+    }
+  },
+  methods: {
+    translate
+  }
+};
+</script>
--- src/CenteredSvg.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <svg width="500" height="500" class="space">
-    <g transform="translate(250, 250)">
-      <slot></slot>
-    </g>
-  </svg>
-</template>
