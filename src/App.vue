9861c74d01e1450e5b6e9230965833d24fdb3d3b
Dave Staab <davestaab@gmail.com>
2019-08-08 10:20:51 -0400

    setup area chart

--- src/App.vue
+++ src/App.vue
@@ -2,55 +2,68 @@
   <div id="app">
     <!-- step 1: the red planet -->
     <svg-chart v-slot="{ innerWidth }">
-      <centered :size="innerWidth">
+      <centered-square :size="innerWidth">
         <circle :r="innerWidth / 2" fill="firebrick"></circle>
-      </centered>
+      </centered-square>
     </svg-chart>
     <br />
     <!-- step 2: render planet -->
     <svg-chart v-slot="{ innerWidth }">
-      <centered :size="innerWidth">
+      <centered-square :size="innerWidth">
         <planet :layers="Earth.layers" :size="innerWidth">
           <text-centered>{{ Earth.name }}</text-centered>
         </planet>
-      </centered>
+      </centered-square>
+    </svg-chart>
+    <br />
+    <!-- area chart -->
+    <svg-chart v-slot="{ innerWidth, innerHeight }">
+      <area-path
+        :width="innerWidth"
+        :height="innerHeight"
+        :data="stockData"
+        :x-scale="stockXScale"
+        :y-scale="stockYScale"
+        :x-accessor="stockXAccessor"
+        :y-accessor="stockYAccessor"
+      ></area-path>
     </svg-chart>
 
     <!-- step 2b: render the sun -->
     <!--    <svg-chart>-->
-    <!--      <centered>-->
+    <!--      <centered-square>-->
     <!--        <planet :layers="Sun.layers" :size="1">-->
     <!--          <text-centered>{{ Sun.name }}</text-centered>-->
     <!--        </planet>-->
-    <!--      </centered>-->
+    <!--      </centered-square>-->
     <!--    </svg-chart>-->
 
     <!-- step 3: render planet in solar system -->
     <!--    <column-svg :size="solarSize" :width="width">-->
     <!--      &lt;!&ndash;      <planet :layers="Sun.layers" :size="10"></planet>&ndash;&gt;-->
     <!--      <space-axis :scale="scale" :size="width" />-->
-    <!--      <solar-system :planets="planets" :scale="scale">-->
-    <!--        <planet slot-scope="{ planet }" :layers="planet.layers" :size="10">-->
-    <!--          <text-centered>{{ planet.name }}</text-centered>-->
-    <!--        </planet>-->
-    <!--      </solar-system>-->
+    <!--          <solar-system :planets="planets" :scale="scale">-->
+    <!--            <planet slot-scope="{ planet }" :layers="planet.layers" :size="10">-->
+    <!--              <text-centered>{{ planet.name }}</text-centered>-->
+    <!--            </planet>-->
+    <!--          </solar-system>-->
     <!--    </column-svg>-->
 
     <!-- step beyond: render 2d animated solar system -->
-    <!--    <centered>-->
+    <!--    <centered-square>-->
     <!--      <solar-system-2d :planets="planets" :size="230" :period="1000">-->
     <!--        <planet slot-scope="{ planet }" :layers="planet.layers" :size="2">-->
     <!--          <text-centered>{{ planet.name }}</text-centered>-->
     <!--        </planet>-->
     <!--      </solar-system-2d>-->
-    <!--    </centered>-->
+    <!--    </centered-square>-->
     <!--    <four-quadrants> </four-quadrants>-->
   </div>
 </template>
 
 <script>
 import SvgChart from './SvgChart';
-import Centered from './Centered';
+import CenteredSquare from './CenteredSquare';
 import ColumnSvg from './ColumnSvg';
 import Planet from './Planet';
 import SolarSystem from './SolarSystem';
@@ -59,9 +72,18 @@ import TextCentered from './TextCentered';
 import { Earth, planets, Sun } from './planets.js';
 import SpaceAxis from './SpaceAxis';
 import FourQuadrants from './FourQuadrants';
+import AreaPath from './AreaPath';
+import aaplStock from 'raw-loader!./data/aapl.csv';
+import { csvParse, autoType } from 'd3-dsv';
+import { scaleLinear, scaleUtc } from 'd3-scale';
+import { max, extent } from 'd3-array';
+import { prop } from './utils';
 
-import { scaleLinear } from 'd3-scale';
-import { max } from 'd3-array';
+const stockData = csvParse(aaplStock, autoType);
+const stockXScale = scaleUtc().domain(extent(stockData, prop('date')));
+const stockYScale = scaleLinear()
+  .domain([0, max(stockData, prop('close'))])
+  .nice();
 
 const scale = scaleLinear()
   .domain([0, max(planets.map(d => d.distance))]) // miles
@@ -70,14 +92,15 @@ const scale = scaleLinear()
 export default {
   components: {
     SvgChart,
-    Centered,
+    CenteredSquare,
     Planet,
     ColumnSvg,
     SolarSystem,
     SolarSystem2d,
     TextCentered,
     SpaceAxis,
-    FourQuadrants
+    FourQuadrants,
+    AreaPath
   },
   data() {
     return {
@@ -87,7 +110,12 @@ export default {
       planets,
       Sun,
       Earth,
-      scale
+      scale,
+      stockXScale,
+      stockYScale,
+      stockData,
+      stockXAccessor: prop('date'),
+      stockYAccessor: prop('close')
     };
   }
 };
