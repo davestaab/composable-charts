49189000109b678240d1c6ef3c2a2ec2c4dbf34e
Dave Staab <davestaab@gmail.com>
2019-08-08 10:17:07 -0400

    eslint reformatting

--- src/ColumnSvg.vue
+++ src/ColumnSvg.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg :width="this.width" :height="size+20*2" class="space">
+  <svg :width="this.width" :height="size + 20 * 2" class="space">
     <g transform="translate(250, 20)">
       <slot></slot>
     </g>
